66bd8a5038fa016e6687a37b50a8f002fe1946ac
boye <bowyeah98@gmail.com>
2026-01-08 09:40:31 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+    $('.firstimg').mouseenter(function(){
+        $('.firstimg').css({
+            'background' : `url('../image/title-img.png') no-repeat center/cover`
+        })
+        $('.imginner').addClass('on')
+    })
+
+
+
+    $('.question').click(function(){
+        $(this).next().slideToggle().siblings('.answer').slideUp()
+    })
+    $('.answer').click(function(){
+        $(this).slideUp()
+    })
+    $('.answer').trigger('slideUp')
+
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= 1200){
+            $('.gotop').fadeIn()
+        }else{
+            $('.gotop').fadeOut()
+        }
+    })
+
+    $('.gotop').click(function(e){
+        e.preventDefault()
+        $('html, body').animate({
+            scrollTop: 0
+        },500)
+
+    })
+
+    
+
+
+
+})
